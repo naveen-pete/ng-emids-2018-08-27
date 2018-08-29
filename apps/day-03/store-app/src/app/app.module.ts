759c8eaba8ec16527980e4b411c5938ccb8b43db
Naveen Pete <naveen.shenoy.blr@gmail.com>
2018-08-29 22:31:18 +0530

    add demo code for component interaction at the end of day 3

--- apps/day-03/store-app/src/app/app.module.ts
+++ apps/day-03/store-app/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { HeaderComponent } from './header/header.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductFormComponent } from './product-form/product-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductDetailComponent,
+    ProductFormComponent
   ],
   imports: [
     BrowserModule,
